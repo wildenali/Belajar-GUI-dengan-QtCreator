d79b6d3f0c419312c7b1495d0fbd41d30091c01a
wildenali <wildeeeeen@gmail.com>
2019-01-18 23:10:25 +0700

    ada statusbar dan progressbar di app login

--- 10_Login_App_StatusBar/build-10_Login_App_StatusBar-Desktop-Debug/moc_mainwindow.cpp
+++ 10_Login_App_StatusBar/build-10_Login_App_StatusBar-Desktop-Debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../11_Login_App_Resource_File/11_Login_App_Resource_File/mainwindow.h"
+#include "../10_Login_App_StatusBar/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
