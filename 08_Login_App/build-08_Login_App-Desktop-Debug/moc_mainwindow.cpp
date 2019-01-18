0e1c19ce7955a43294c59146cd3f9351da5f37d5
wildenali <wildeeeeen@gmail.com>
2019-01-18 23:21:44 +0700

    aplikasi login

--- 08_Login_App/build-08_Login_App-Desktop-Debug/moc_mainwindow.cpp
+++ 08_Login_App/build-08_Login_App-Desktop-Debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../09_Login_App_Display_Image/09_Login_App_Display_Image/mainwindow.h"
+#include "../08_Login_App/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- 08_Login_App/build-08_Login_App-Desktop-Debug/moc_second_dialog.cpp
+++ 08_Login_App/build-08_Login_App-Desktop-Debug/moc_second_dialog.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../09_Login_App_Display_Image/09_Login_App_Display_Image/second_dialog.h"
+#include "../08_Login_App/second_dialog.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
